d6916444b602a5fe7d1be30080732a83a7a9be72
Kateryna Stelmakhova <katya.stelmakhova@gmail.com>
2024-10-03 22:22:42 +0200

    test3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,6 @@ export default function Home() {
     <div>
         <h1>Rick and Morty</h1>
         <p>Welcome to the Rick and Morty website!</p>
-        <p>Here you can find all the information about the show.</p>
-        <p>Enjoy!</p>
     </div>
   );
 }
